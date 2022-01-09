65c0bc12eac0d6a093681c0f1f52bc9ceadc4f4b
Anand chauhan <57872259+andc877@users.noreply.github.com>
2022-01-09 08:58:28 -0800

    ll.cpp

---
+++ ll.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<windows.h>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    system("Color 0A");
+    cout<<"anand chauhan";
+     //system("Color E4");
+    return 0;
+
+}
